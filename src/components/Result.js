af2cf527bb2dd4be9b2b3a62a879832ad4704c9a
Rhythm vashist <44883002+rhythmvashist@users.noreply.github.com>
2020-10-29 05:03:21 -0700

    initial commit

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,80 +1,79 @@
-import React,{useState,useEffect} from 'react'
-import "./style/Result.css"
-import VideoCard from './VideoCard'
+import React, { useState, useEffect } from "react";
+import "./style/Result.css";
+import VideoCard from "./VideoCard";
 import axios from "../axios";
-import YouTube from 'react-youtube';
-import  PageCount from './PageCount'
+import YouTube from "react-youtube";
+import PageCount from "./PageCount";
 
-function Result({selectedOption}) {
-    const [movies,setMovies] = useState([]);
-    const [page,setPage] = useState(1);
+function Result({ selectedOption }) {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
 
-    // const opts = {
-    //     height: '390',
-    //     width: '640',
-    //     playerVars: {
-    //       // https://developers.google.com/youtube/player_parameters
-    //       autoplay: 1,
-    //     },
-    //   };
-    
-    useEffect(() => {
-        async function fetchData( ){
-            const request = await axios.get(`${selectedOption}&page=${page}`);
-            console.log(request);
-            setMovies(request.data.results)
-            return  request;
-        }
-        fetchData();
-        // if the genre changes then the page is reset to 1 
-        setPage(1);
-    },[selectedOption])
+  // const opts = {
+  //     height: '390',
+  //     width: '640',
+  //     playerVars: {
+  //       // https://developers.google.com/youtube/player_parameters
+  //       autoplay: 1,
+  //     },
+  //   };
 
-    useEffect(() => {
-        async function fetchData( ){
-            const request = await axios.get(`${selectedOption}&page=${page}`);
-            console.log(request);
-            setMovies(request.data.results)
-            return  request;
-        }
-        fetchData();
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`${selectedOption}&page=${page}`);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+    // if the genre changes then the page is reset to 1
+    setPage(1);
+  }, [selectedOption]);
 
-        try {
-            // trying to use new API - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
-            window.scroll({
-              top: 0,
-              left: 0,
-              behavior: 'smooth',
-            });
-          } catch (error) {
-            // just a fallback for older browsers
-            window.scrollTo(0, 0);
-          }
-    },[page])
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`${selectedOption}&page=${page}`);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
 
-    // const handleClick=(movie)=>{
-    //     if(trailer){
-    //         setTrailer('');
-    //     }
-    //     else{
-    //         movieTrailer(movie?.name ||"")
-    //         .then((url) => {
-    //             const 
-    //         })
-    //     }
+    try {
+      // trying to use new API - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    } catch (error) {
+      // just a fallback for older browsers
+      window.scrollTo(0, 0);
+    }
+  }, [page]);
 
-    // }
+  // const handleClick=(movie)=>{
+  //     if(trailer){
+  //         setTrailer('');
+  //     }
+  //     else{
+  //         movieTrailer(movie?.name ||"")
+  //         .then((url) => {
+  //             const
+  //         })
+  //     }
 
-    return (
-        <div>
-            <div className="results">
-                {movies.map(movie => (
-                    <VideoCard key={movie.id} movie={movie}/>
-                ))}            
-            </div>
-            <PageCount pageNumber={page} setPageNumber={setPage}/>
-        </div>
-    )
+  // }
+
+  return (
+    <div>
+      <div className="results">
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <PageCount pageNumber={page} setPageNumber={setPage} />
+    </div>
+  );
 }
 
-export default Result
+export default Result;
