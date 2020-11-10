00fc65efcd9a022d25a845dfe88aabe903858663
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-11-09 20:20:52 -0800

    started working on watchllist page

---
+++ src/components/Watchlist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Watchlist() {
+    return (
+        <div>
+            <h1>
+            Watchlist
+        </h1>
+            
+        </div>
+    )
+}
+
+export default Watchlist
